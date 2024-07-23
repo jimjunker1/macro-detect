dbb3bb7db7e6fccb8ac7e8b129d02eb63e9e7499
Jim Junker <james.junker1@gmail.com>
2024-07-22 22:51:59 -0500

    updating BB regression model

---
+++ code/train.py
@@ -0,0 +1,16 @@
+# import the necessary packages
+from pyimagesearch import config
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import load_img
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import os
+
